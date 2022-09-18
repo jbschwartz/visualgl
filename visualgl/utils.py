b3f83ba46076d5a3e420f455d61d556f6a06dab4
James Schwartz <jbschwartz@users.noreply.github.com>
2022-09-18 10:32:52 -0400

    Remove dead code.

--- visualgl/utils.py
+++ visualgl/utils.py
@@ -1,32 +1,5 @@
-import math
 from numbers import Number
 from typing import Union
-
-
-def clamp_angle(angle):
-    """
-    Clamp angle to the range (-pi, pi]
-    """
-    revolution = 2 * math.pi
-
-    while angle > math.pi:
-        angle -= revolution
-    while angle <= -math.pi:
-        angle += revolution
-
-    return angle
-
-
-def safe_acos(value):
-    """
-    Checks to make sure that small floating point representation "errors" (e.g. 1.00000000002) do not raise ValueErrors
-    """
-    if math.isclose(value, 1):
-        return 0
-    elif math.isclose(value, -1):
-        return math.pi
-    else:
-        return math.acos(value)
 
 
 def raise_if(should_raise: bool, exception_type: Exception):
