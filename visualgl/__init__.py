cf92034493820d3603d082ea7571f5cfac90b54c
James Schwartz <jbschwartz@users.noreply.github.com>
2022-10-01 14:33:00 -0400

    Create Scene subpackage.

--- visualgl/__init__.py
+++ visualgl/__init__.py
@@ -1,11 +1,9 @@
 import logging
 
-from .ambient_light import AmbientLight
 from .application import Application
 from .camera.camera_controller import CameraController
 from .filetypes.stl.stl_parser import STLParser
 from .renderer import Renderer
-from .scene import Scene
 from .settings import settings
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
---
+++ visualgl/scene/__init__.py
@@ -0,0 +1,4 @@
+"""Scene subpackage."""
+
+from .ambient_light import AmbientLight
+from .scene import Scene
